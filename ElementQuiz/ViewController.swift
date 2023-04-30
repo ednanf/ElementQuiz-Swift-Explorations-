27df3c8082ff22a511cfbbfb04a825912a1699cc
Ednan <52186220+ednanf@users.noreply.github.com>
2023-04-30 09:39:22 -0300

    Part 3 - start

--- ElementQuiz/ViewController.swift
+++ ElementQuiz/ViewController.swift
@@ -8,16 +8,57 @@
 import UIKit
 
 class ViewController: UIViewController {
+
 // MARK: - IBOutlets
     @IBOutlet var imageView: UIImageView!
     @IBOutlet var answerLabel: UILabel!
     
+    
+// MARK: - Properties
+    let elementList = ["Carbon", "Gold", "Chlorine", "Sodium"]
+    var currentElementIndex = 0 // Variable to keep track of the currently selected element.
+    
+    
 // MARK: - Setup
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        updateElement()
     }
-
-
+    
+// MARK: - IBActions
+    
+    @IBAction func showAnswer(_ sender: Any) {
+    }
+    
+    
+    @IBAction func next(_ sender: Any) {
+    }
+    
+    
+// MARK: - Functions
+    
+    func updateElement() {
+        let elementName = elementList[currentElementIndex] // This constant accesses the element name from the list using the currentElementIndex property as the index.
+        let image = UIImage(named: elementName) // This line creates a new UIImage instance by looking for an image in the Asset Catalog with a matching name.
+        imageView.image = image // This line sets the image of the image view to the newly created image instance.
+        
+        answerLabel.text = "?" // This line sets the text of the answer label (using the text property of the UILabel type) to a question mark.
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }
-
